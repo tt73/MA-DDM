b47cfa76581dbebb8fcb9826eba2461ca434db3f
tt73 <tt73@njit.edu>
2022-02-24 09:21:04 -0500

    view

--- Cpp/Notes/Example2D/run.m
+++ Cpp/Notes/Example2D/run.m
@@ -1,9 +1,25 @@
 % System Command
 
-status = system('ls -l')
+[status,cmd] = system('ls -l')
 
 % MATLAB + PETSc
 % Im reading this webpage: https://petsc4py.readthedocs.io/en/stable/manual/matlab/
 
-status = system('mpiexec ./poisson -mat_view :A.m:ascii_matlab -vec_view :b.m:ascii_matlab')
 
+
+% status = system('mpiexec ./poisson -mat_view :A.m:ascii_matlab -vec_view :b.m:ascii_matlab')
+
+[status,cmd] = system('which mpiexec')
+
+
+[~,pth] = system('echo $PATH')
+
+pth = strsplit(pth,':')
+
+
+
+[status,cmd] = system('mpiexec ./poisson -h')
+
+[~,pth] = system('echo $PATH')
+
+pth = strsplit(pth,':')
