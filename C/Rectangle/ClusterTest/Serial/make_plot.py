17a97555cc4ae88d4b3c7d4292a3e85272d0a985
tt73 <tt73@njit.edu>
2022-06-25 07:52:49 -0700

    added serial plot

--- C/Rectangle/ClusterTest/Serial/make_plot.py
+++ C/Rectangle/ClusterTest/Serial/make_plot.py
@@ -1,9 +1,11 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import sys
+import seaborn as sns
 
-f = open("out_test1")
-N = 7
+colors=sns.color_palette("rocket",3)
+
+f = open("out_test1b")
+N = 9
 Ns = np.zeros(N,dtype=int)
 times = np.zeros((3,N),dtype=float)
 for i in range(N):
@@ -16,10 +18,10 @@ for i in range(N):
       f.readline() # Iters
    Ns[i] = 100 + i*50
 
-fig = plt.figure(1, figsize=(5,5))
-plt.plot(Ns,times[0],'-', label='P1',marker='o',mfc='w')
-plt.plot(Ns,times[1],':', label='P2',marker='s',mfc='w')
-plt.plot(Ns,times[2],'--',label='P3',marker='^',mfc='w')
+fig = plt.figure(1, figsize=(6,6))
+plt.plot(Ns,times[0],'-', label='P1',marker='o',mfc='w',color=colors[0])
+plt.plot(Ns,times[1],':', label='P2',marker='s',mfc='w',color=colors[1])
+plt.plot(Ns,times[2],'--',label='P3',marker='^',mfc='w',color=colors[2])
 plt.xlabel('$N$')
 plt.ylabel('Time (sec)')
 plt.savefig('test.png')
