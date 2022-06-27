69acbfb30487c87a939959923add90c5fe3390e2
tt73 <tt73@njit.edu>
2022-06-27 05:29:17 -0700

    updated latex table generator

--- C/Rectangle/ClusterTest/Serial/make_plot.py
+++ C/Rectangle/ClusterTest/Serial/make_plot.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
 
+plt.rcParams.update({'font.size' : 14})
 colors=sns.color_palette("rocket",3)
 
 f = open("out_test1b")
@@ -19,15 +20,18 @@ for i in range(N):
    Ns[i] = 100 + i*50
 
 fig = plt.figure(1, figsize=(6,6))
-plt.plot(Ns,times[0],'-', label='P1',marker='o',mfc='w',color=colors[0])
-plt.plot(Ns,times[1],':', label='P2',marker='s',mfc='w',color=colors[1])
-plt.plot(Ns,times[2],'--',label='P3',marker='^',mfc='w',color=colors[2])
+plt.plot(Ns,times[0],'-', label='P1',marker='o',mfc='w',color=colors[0],ms=8)
+plt.plot(Ns,times[1],':', label='P2',marker='s',mfc='w',color=colors[1],ms=8)
+plt.plot(Ns,times[2],'--',label='P3',marker='^',mfc='w',color=colors[2],ms=8)
 plt.tick_params(direction='in',right=True,top=True)
 plt.tick_params(labelsize=14)
+plt.tick_params(direction='in',which='minor', length=5, bottom=True, top=True, left=True, right=True)
+plt.tick_params(direction='in',which='major', length=10, bottom=True, top=True, left=True, right=True)
 plt.tick_params(labelbottom=True,labeltop=False,labelright=False,labelleft=True)
 plt.xlabel('$N$')
 plt.ylabel('Time (sec)')
+plt.yticks(np.arange(0,1200,120))
 plt.legend(fontsize=14)
 plt.savefig('serial.png',dpi=300,bbox_inches='tight')
-# plt.show()
+plt.show()
 
--- C/Rectangle/ClusterTest/Set4/make_latex_table.py
+++ C/Rectangle/ClusterTest/Set4/make_latex_table.py
@@ -1,13 +1,10 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import seaborn as sns
 
-colors=sns.color_palette("rocket",3)
-
-filename = 'out_test1'
+filename = 'out_test3'
 
 f = open(filename)
-N = 5
+N = 6
 op = np.zeros(N,dtype=float)
 times = np.zeros((3,N),dtype=float)
 iters = np.zeros((3,N),dtype=int)
@@ -27,22 +24,8 @@ print(iters)
 for i in range(3):
    print("P{:d} & ".format(i),end='')
    for j in range(N):
-      print("{:.3f} & {:d} & ".format(times[i][j],iters[i][j]),end='')
-   print('')
-
-
-
-
-# fig = plt.figure(1, figsize=(6,6))
-# plt.plot(Ns,times[0],'-', label='P1',marker='o',mfc='w',color=colors[0])
-# plt.plot(Ns,times[1],':', label='P2',marker='s',mfc='w',color=colors[1])
-# plt.plot(Ns,times[2],'--',label='P3',marker='^',mfc='w',color=colors[2])
-# plt.tick_params(direction='in',right=True,top=True)
-# plt.tick_params(labelsize=14)
-# plt.tick_params(labelbottom=True,labeltop=False,labelright=False,labelleft=True)
-# plt.xlabel('$N$')
-# plt.ylabel('Time (sec)')
-# plt.legend(fontsize=14)
-# plt.savefig('serial.png',dpi=300,bbox_inches='tight')
-# plt.show()
-
+      print("{:.3f} & {:d}".format(times[i][j],iters[i][j]),end='')
+      if (j<N-1):
+         print(" & ",end='')
+      else:
+         print(" \\\\")
