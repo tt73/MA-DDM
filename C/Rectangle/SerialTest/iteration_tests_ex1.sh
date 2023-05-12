ed38473d8e5d9c7d657ce29119734e6fa46e9ff3
Tadanaga Takahashi <tt73@login-1.lochness.lan>
2023-05-12 11:27:46 -0400

    ran tests up to 5 by 5

--- C/Rectangle/SerialTest/iteration_tests_ex1.sh
+++ C/Rectangle/SerialTest/iteration_tests_ex1.sh
@@ -1,11 +1,12 @@
+#!/bin/bash -l
 #SBATCH -J ex1-iters
-#SBATCH -p public
-#SBATCH -p dms-cpu
+#SBATCH -p lowpriority
+#SBATCH --constraint=avx512
 #SBATCH --mail-user tt73@njit.edu
 #SBATCH --mail-type=END
 #SBATCH -A tt73
 #SBATCH -t 24:0:0
-#SBATCH --mem=0
+#SBATCH --mem=0G
 #SBATCH -N 1
 
 ## These are the tests for Example 1 on [-L,L]^2
@@ -24,14 +25,16 @@ module load gnu8 mpich petsc/3.12.0
 rm -f out
 
 for Nd in {2,4,6,8,9,16,25,36}
-for op in {0.1,0.2,0.3,0.4}
 do
-   for limit in {0.5,1.0,1.5,2.0}
+   for op in {0.1,0.2,0.3,0.4}
    do
-      for h in {0.05,0.01}
+      for limit in {0.5,1.0,1.5,2.0}
       do
-         N=$(echo "scale = 0; 2*$limit/$h+1" | bc)
-         mpiexec -np $Nd ../maddm -N $N -problem ex1 -xmin -$limit -xmax $limit -ymin -$limit -ymax $limit -op $op -init_type coarse -coarseness 4 -snes_converged_reason >> out
+         for h in {0.05,0.01}
+         do
+            N=$(echo "scale = 0; 2*$limit/$h+1" | bc)
+            mpiexec -np $Nd ../maddm -N $N -problem ex1 -xmin -$limit -xmax $limit -ymin -$limit -ymax $limit -op $op -init_type coarse -coarseness 4 -snes_converged_reason >> out
+         done
       done
    done
 done
